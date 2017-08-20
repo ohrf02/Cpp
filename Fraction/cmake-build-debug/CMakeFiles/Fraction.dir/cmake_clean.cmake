76ec370a6ce1a1e8e71b3471d93a6dca5ea4248e
or ferzdhant <ohrf02@gmail.com>
2017-08-20 11:32:29 +0300

    Fraction was added

---
+++ Fraction/cmake-build-debug/CMakeFiles/Fraction.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fraction.dir/main.cpp.obj"
+  "CMakeFiles/Fraction.dir/Fraction.cpp.obj"
+  "Fraction.pdb"
+  "Fraction.exe"
+  "Fraction.exe.manifest"
+  "libFraction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
